86316ac7329b610e9e7c457ae08334f982c7e123
Brandon Heller <brandonh@stanford.edu>
2013-04-12 02:03:05 -0700

    Add code update to wiki update

--- update.sh
+++ update.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+# Update wiki pages
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
+git pull
+# Update code, assumed to be cloned at same dir depth
+cd ../onstutorial
 git pull
